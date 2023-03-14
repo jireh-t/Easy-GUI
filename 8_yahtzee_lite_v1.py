1e002eeda6d28366fc868b8c3f5dd5d83556493b
jireh-t <98923573+jireh-t@users.noreply.github.com>
2023-03-15 10:26:27 +1300

    update

--- 8_yahtzee_lite_v1.py
+++ 8_yahtzee_lite_v1.py
@@ -4,52 +4,57 @@
 import easygui
 import random
 
-dice = [1, 2, 3, 4, 5, 6]
+# List of numbers in my dice
+dice = ["1", "2", "3", "4", "5", "6"]
 
+# Welcome the user and give the option to play or not
+again = easygui.buttonbox("Welcome, would you like to play?", "Welcome",
+                          choices=["Yes", "No"])
+
+# Loops the game as long as the user wants to keep playing
 while True:
     turns = 0
-    again = easygui.buttonbox("Welcome, would you like to play?", "Welcome",
-                              choices=["Yes", "No"])
+
+    # End the game if the user wants to exit
     if again == "No":
         easygui.msgbox("Goodbye!", "Exit")
         break
 
-    while True:
+    while turns != 3:
         roll = []
 
+        # Shuffles and rolls dice 5 times to get 5 numbers
         for num in range(0, 5):
             random.shuffle(dice)
             roll.append(dice[0])
 
-        roll.sort()
+        roll.sort()  # Sort the numbers from lowest to highest
 
-        choice = easygui.buttonbox(f"You rolled {roll}\n\n"
+        # Give the user 2 choices
+        choice = easygui.buttonbox(f"You rolled {', '.join(roll)}\n\n"
                                    f"Choose:", choices=["Roll again", "Stick"])
-        turns += 1
+
+        turns += 1  # Count the number of turns they've had
 
         if choice == "Stick":
-            counter = 0
-            for option in roll:
-                repeats = roll.count(option)
+            counter = 0  # Keep track of the maximum repeats
+            for option in roll:  # Loops through every number
+                repeats = roll.count(option)  # Counts how many repeats
                 if repeats > counter:
-                    repeats = counter
+                    counter = repeats  # Replaces the max if higher
 
+            # Give message and result depending on how many repeats
             if counter == 5:
-                easygui.msgbox(f"{roll}\n\n Yahtzee!")
+                easygui.msgbox(f"{', '.join(roll)}\n\n Yahtzee!")
             elif counter == 4:
-                easygui.msgbox(f"{roll}\n\n Four of a kind!")
+                easygui.msgbox(f"{', '.join(roll)}\n\n Four of a kind!")
             elif counter == 3:
-                easygui.msgbox(f"{roll}\n\n Three of a kind!")
+                easygui.msgbox(f"{', '.join(roll)}\n\n Three of a kind!")
             else:
                 easygui.msgbox("Better luck next time!")
 
-
+        # Asks the user to play again after 3 turns
         if turns == 3:
             again = easygui.buttonbox("You've had 3 turns. Do you want to play "
                                       "another round?", "Goodbye",
                                       choices=["Yes", "No"])
-
-
-
-
-
